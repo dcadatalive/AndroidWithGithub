10fbcae82de5ae3198b3cc0f252514c5079b31dc
david <dcadatalive@gmail.com>
2023-02-18 17:27:28 -0800

    update two files, one is the layout, the other is the main activity

--- app/src/main/java/com/davidallen/androidwithgithub/MainActivity.kt
+++ app/src/main/java/com/davidallen/androidwithgithub/MainActivity.kt
@@ -2,6 +2,9 @@ package com.davidallen.androidwithgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Our first Project on Github
+
+        val text = findViewById<TextView>(R.id.text_View)
+        val btn = findViewById<Button>(R.id.btn)
+        val image = findViewById<ImageView>(R.id.image)
+
+
+
+
 
 
 
